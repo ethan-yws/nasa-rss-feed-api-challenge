0dabd14c40c8df0eef70cc275c991e60582e2587
ethan-yws <38035032+ethan-yws@users.noreply.github.com>
2022-01-30 15:47:50 +1100

    chore: update comments

--- src/api/services/getPayload.service.js
+++ src/api/services/getPayload.service.js
@@ -3,6 +3,12 @@ const { rssUrl } = require('../../config/default.config');
 const { formatPayload } = require('../services/payload.format.service');
 const loggerService = require('../common/logger/logger');
 
+/**
+ * Get the payload we need from rss data in given pubDate order
+ *
+ * @param {string} order
+ * @returns {Object} payload we need
+ */
 const getPayload = async (order) => {
   loggerService.info('>> fetching RSS data to js object :::');
   const rssData = await getRSSFeed(rssUrl);
